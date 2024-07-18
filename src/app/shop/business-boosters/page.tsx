fac565acb375b92ffc91c741a98c0747caa5327c
Safe-net-user <146375103+Safe-net-user@users.noreply.github.com>
2024-07-18 13:23:43 +0000

    dfgh

--- src/app/shop/business-boosters/page.tsx
+++ src/app/shop/business-boosters/page.tsx
@@ -14,25 +14,25 @@ export default async function BusinessBoostersPage() {
     <main>
       <HeaderSection title="Business Boosters" />
       <div className="grid grid-cols-1 gap-8 px-6 py-16 lg:px-24">
-        {businessBoosters?.map((businessBooster) => (
+       
           <div
             className="flex w-full flex-col md:flex-row"
-            key={businessBooster.id}
+           
           >
             <Image
               className="w-1/2"
-              src={businessBooster.image}
-              alt={businessBooster.alt}
+              src={'d'}
+              alt={}
               width={1000}
               height={1000}
             />
             <div className="w-1/2 bg-gray-100 p-4">
-              <h4 className="text-[20px] font-bold">{businessBooster.title}</h4>
+              <h4 className="text-[20px] font-bold">{}</h4>
               <h3 className="text-[24px]">
                 {Intl.NumberFormat('fr-FR', {
                   style: 'currency',
                   currency: 'EUR',
-                }).format(businessBooster.price)}
+                }).format()}
               </h3>
               <p
                 dangerouslySetInnerHTML={{
@@ -50,7 +50,7 @@ export default async function BusinessBoostersPage() {
               </div>
             </div>
           </div>
-        ))}
+      
       </div>
     </main>
   );
