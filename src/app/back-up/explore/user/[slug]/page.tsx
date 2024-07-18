fb172d0cc12f0bbca695438565a7f16cf57de21c
Safe-net-user <146375103+Safe-net-user@users.noreply.github.com>
2024-07-18 12:43:44 +0000

    fr

--- src/app/back-up/explore/user/[slug]/page.tsx
+++ src/app/back-up/explore/user/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import ProfessionalDiscoverCard from '@/components/back-up/Explorer/professional';
 import { Card, CardContent } from '@/components/ui/card';
 import {
   Carousel,
@@ -10,16 +9,14 @@ import {
 
 export default function ProfessionalPage({
   params: { slug },
-}: Readonly<{ params: { slug: string } }>) {
+}: Readonly<{ params: { slug: string  } }>) {
   return (
     <main>
       <Carousel className="ml-52 w-full max-w-xs">
         <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
             <CarouselItem key={index}>
-              <div className="p-1">
-                <ProfessionalDiscoverCard/>
-              </div>
+
             </CarouselItem>
           ))}
         </CarouselContent>
